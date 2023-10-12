26d7d5fada0fb7d7652c465b27ed8c3e3ecb65bb
CappieGold <carpentier.jeremy74@gmail.com>
2023-10-12 05:12:15 -0700

    c program C puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
  * main - Entry point
+ *
+ * Description: '"c programme with puts"'
+ *
  * return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
