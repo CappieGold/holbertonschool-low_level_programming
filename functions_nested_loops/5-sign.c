c12dc3480974951711b9b9bf5d3b031079b1d7ac
CappieGold <carpentier.jeremy74@gmail.com>
2023-10-17 06:44:00 -0700

    task 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - return -1, 0, 1 if n inferieure, egale ou sup a 0
+ * @n: output num
+ * Return: -1, 0, 1 (Success)
+ */
+
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	return (0);
+}
