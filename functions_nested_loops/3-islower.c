bca2e789ed26967fdeb7bef319e0b317cd0818ed
CappieGold <carpentier.jeremy74@gmail.com>
2023-10-17 05:46:37 -0700

    c return 1 if is lowercase

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,13 +2,11 @@
 
 /**
  * _islower - c is 1 if is not 0
- *
+ *@c: input charactere
  * Return: Always 0 (Success)
  */
 
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122)
-		return (1);
-	return (0);
+	return (c >= 'a' && c <= 'z');
 }
