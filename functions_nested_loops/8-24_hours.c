bd8c56a12040b572bfab9965de15c9b1b96398ef
CappieGold <carpentier.jeremy74@gmail.com>
2023-10-18 01:19:44 -0700

    print every hour and minute

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - display every hour and minute
+ */
+
+void jack_bauer(void)
+{
+	int heure, minute;
+
+	for (heure = 0; heure <= 23; heure++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar(heure / 10 + '0');
+			_putchar((heure % 10) + '0');
+			_putchar(':');
+			_putchar(minute / 10 + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
