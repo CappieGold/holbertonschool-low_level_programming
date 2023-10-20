c57319a144ee063c235846d5f6b54ba341c88c91
CappieGold <carpentier.jeremy74@gmail.com>
2023-10-20 03:47:05 -0700

    print line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - draw line
+ * @n: variable number of times
+ */
+
+void print_line(int n)
+{
+	int line;
+
+	if (n > 0)
+	{
+		for (line = 0; line < n; line++)
+			_putchar('-');
+	}
+	_putchar('\n');
+}
+
