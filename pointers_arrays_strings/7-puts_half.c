3272ed38f84119b2a9d24abfff8be745f5b82bea
CappieGold <carpentier.jeremy74@gmail.com>
2023-10-24 05:08:38 -0700

    prints half of string

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -13,7 +13,7 @@ void puts_half(char *str)
 	while (str[lenght] != '\0')
 		lenght++;
 
-	for (index = lenght / 2; index < lenght; index++)
+	for (index = lenght / 2; index <= lenght; index++)
 	{
 		if (index % 2 != 0)
 			_putchar(str[index] - 1 / 2);
