5c37fb2e64c1350a5ac4c5eef867df118b3691e1
CappieGold <carpentier.jeremy74@gmail.com>
2023-10-24 07:50:17 -0700

    copy string

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -11,10 +11,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
 
-	while (src[index] != '\0')
+	while (src[index])
 	{
 		dest[index] = src[index];
 		index++;
 	}
+	*dest = '\0';
 	return (dest);	
 }
