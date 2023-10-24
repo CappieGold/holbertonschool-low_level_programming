fca636a560f76c501a3b9fab611e7b500dfd63ff
CappieGold <carpentier.jeremy74@gmail.com>
2023-10-24 05:43:16 -0700

    prints half of string

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -18,10 +18,7 @@ void puts_half(char *str)
 
 	for (index = (lenght / 2); index < lenght; index++)
 	{
-		if (index % 2 != 0)
-			_putchar(str[index]);
-		else
-			_putchar(str[index]);
+		_putchar(str[index]);
 	}
 	_putchar('\n');
 }
