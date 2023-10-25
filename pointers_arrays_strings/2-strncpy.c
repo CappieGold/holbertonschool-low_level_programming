c4c6cdfd78e0c4aac5a9d1af1aaff45327341594
CappieGold <carpentier.jeremy74@gmail.com>
2023-10-25 04:15:02 -0700

    strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strncpy - Write a function that copies a string.
+ * @dest: destination
+ * @src: source
+ * @n: want to copy
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0;
+
+	while (index < n && src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	for (; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
+}
