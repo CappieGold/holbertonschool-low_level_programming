f43f8faa11343e8bdabd4366e9d6f87a489c992d
CappieGold <carpentier.jeremy74@gmail.com>
2023-11-08 16:59:23 +0000

    strncat

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *string_nconcat - Write a function that concatenates two strings.
+ * @s1: string after what i want concatenate
+ * @s2: string concatenate after s1
+ * @n: number of charactere i want concatenate of s2 after s1
+ * Return: pointer to the concatenate
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *concatenates;
+	unsigned int lenght = n;
+	unsigned int index = 0;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	for (index = 0; s1[index]; index++)
+	{
+		lenght++;
+	}
+
+	concatenates = malloc(sizeof(char) * (lenght + 1));
+
+	if (concatenates == NULL)
+	{
+		return (NULL);
+	}
+
+	lenght = 0;
+
+	for (index = 0; s1[index]; index++)
+	{
+		concatenates[lenght++] = s1[index];
+	}
+
+	for (index = 0; s2[index] && index < n; index++)
+	{
+		concatenates[lenght++] = s2[index];
+	}
+
+	concatenates[lenght] = '\0';
+
+	return (concatenates);
+}
