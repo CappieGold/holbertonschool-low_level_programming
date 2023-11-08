2d8faa4a078a875bd8d2457327067fe178af3f87
CappieGold <carpentier.jeremy74@gmail.com>
2023-11-08 17:12:33 +0000

    strncat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,44 +12,30 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *concatenates;
-	unsigned int lenght = n;
-	unsigned int index = 0;
+	unsigned int lenght = n, index = 0;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
 	for (index = 0; s1[index]; index++)
-	{
 		lenght++;
-	}
 
 	concatenates = malloc(sizeof(char) * (lenght + 1));
 
 	if (concatenates == NULL)
-	{
 		return (NULL);
-	}
 
 	lenght = 0;
 
 	for (index = 0; s1[index]; index++)
-	{
 		concatenates[lenght++] = s1[index];
-	}
 
 	for (index = 0; s2[index] && index < n; index++)
-	{
 		concatenates[lenght++] = s2[index];
-	}
 
 	concatenates[lenght] = '\0';
-
 	return (concatenates);
 }
