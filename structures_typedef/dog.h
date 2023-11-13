dfba27006bca300e7be7ea681370ccb80519ecb9
CappieGold <carpentier.jeremy74@gmail.com>
2023-11-13 14:24:28 +0000

    task1 new type struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * my file dog.h
+ * define a new type struct dog with new elements
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
