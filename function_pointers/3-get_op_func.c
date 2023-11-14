36ee2c84b0cfbd4c9bde3f1c6aad269204a23bb0
CappieGold <carpentier.jeremy74@gmail.com>
2023-11-14 15:03:47 +0000

    task3 calculator

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -21,13 +21,9 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i;
 
-	i = 0;
-	while (ops[i].f != NULL)
-	{
-		if (*s == *(ops[i].op) && s[1] == '\0')
-			return (ops[i].f);
-		i++;
-	}
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+        i++;
+    return (ops[i].f);
 
 	printf("Error\n");
 	exit(99);
