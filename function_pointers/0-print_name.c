5221ddd6a29b0b726c23d2633af321289007d288
CappieGold <carpentier.jeremy74@gmail.com>
2023-11-14 11:58:19 +0000

    task1 print name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - Write a function that prints a name.
+ * @name: name of the person
+ * @f: pointer of function printname
+ * @char: argument of f
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+	{
+		return;
+	}
+
+	f(name);
+}
