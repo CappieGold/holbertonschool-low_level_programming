635a4b1e94428cc2aa5d51c32efd3dcf63aa2288
CappieGold <carpentier.jeremy74@gmail.com>
2023-11-15 14:07:33 +0000

    task1 print num

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -5,7 +5,7 @@
 /**
  * print_numbers - Write a function that prints numbers, followed by a new line
  * @separator: separator of my arguments
- * n: number of argument
+ * @n: number of argument
 */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
