59680f217ce1f00d417c285fde1fb1adaec0ed43
CappieGold <carpentier.jeremy74@gmail.com>
2023-11-28 12:32:07 +0000

    task4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_list - free the list lits_t
+ * @head: Pointer to the first node
+*/
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	if (head == NULL)
+	{
+		return;
+	}
+
+	if (head != NULL)
+	{
+		temp = head;
+		free(head->str);
+		head = head->next;
+		free(temp);
+	}
+}
