c1c5796aad547f87bad839645cfac3a3c6692652
CappieGold <carpentier.jeremy74@gmail.com>
2023-12-05 15:46:39 +0000

    task1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -24,7 +24,8 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (lenght = 0; text_content[lenght] != '\0'; lenght++);
+	for (lenght = 0; text_content[lenght] != '\0'; lenght++)
+	;
 
 	nWrite = write(fd, text_content, lenght);
 
