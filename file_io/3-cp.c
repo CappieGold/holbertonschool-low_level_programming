f754aba22346932025e017fb02edda64c867bac9
CappieGold <carpentier.jeremy74@gmail.com>
2023-12-06 13:33:18 +0000

    task3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -12,7 +12,7 @@
 
 int main(int ac, char **av)
 {
-	int fd_src, fd_dest, nRead, nWrite;
+	int fd_from, fd_to, nRead, nWrite;
 	char buf[1024];
 
 	if (ac != 3)
@@ -20,40 +20,41 @@ int main(int ac, char **av)
 		dprintf(2, "Usage: %s file_from file_to\n", av[0]);
 		exit(97);
 	}
-	fd_src = open(av[1], O_RDONLY);
-	if (fd_src == -1)
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	fd_dest = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_dest == -1)
+	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", av[2]);
-		close(fd_src);
+		close(fd_from);
 		exit(99);
 	}
-	while ((nRead = read(fd_src, buf, 1024)) > 0)
+	while ((nRead = read(fd_from, buf, 1024)) > 0)
 	{
-		nWrite = write(fd_dest, buf, nRead);
+		nWrite = write(fd_to, buf, nRead);
 		if (nWrite != nRead || nWrite == -1)
 		{
 			dprintf(2, "Error: Can't write to %s\n", av[2]);
-			close(fd_src);
-			close(fd_dest);
+			close(fd_from);
+			close(fd_to);
 			exit(99);
 		}
 	}
 	if (nRead == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		close(fd_src);
-		close(fd_dest);
+		close(fd_from);
+		close(fd_to);
 		exit(98);
 	}
-	if (close(fd_src) == -1 || close(fd_dest) == -1)
+	if (close(fd_from) == -1 || close(fd_to) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", (close(fd_src) == -1) ? fd_src : fd_dest);
+		dprintf(2, "Error: Can't close fd %d\n", (close(fd_from) == -1) ? fd_from
+		: fd_to);
 		exit(100);
 	}
 	return (0);
